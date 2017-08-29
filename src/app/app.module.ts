746770d5941351ecf3eff860936f00aec88bcfdf
Matheus <matheuspq79@outlook.com.br>
2017-08-28 23:10:45 -0300

    terceiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { AboutPage } from './../pages/about/about';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AboutPage } from './../pages/about/about';
+  //   import { AboutPage } || Importando a classe AboutPage...
+  //   from './../pages/about/about'; || ... localizada neste caminho
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -16,7 +18,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      //activator: 'ripple',
+      backButtonText: 'Voltar'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
